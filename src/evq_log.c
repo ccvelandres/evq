41154bb0643572e2b63d29ec82beba1f31c7794c
ccvelandres <ccvelandres@gmail.com>
2021-06-19 14:21:02 +0800

    evq: Update evq logging
    Moved task cmake args to cmake presets
    Added implementation to default evq_log function

--- src/evq_log.c
+++ src/evq_log.c
@@ -16,6 +16,26 @@ void            evq_log_set_level(const evq_log_level_t level) { evq_log_level =
 void __attribute__((weak)) evq_log(evq_log_level_t level, const char *fmt, ...)
 {
     // override this function
+
+    va_list  args;
+    char    *buf = NULL;
+    uint32_t len = 0;
+    // Compute needed buffer length
+    va_start(args, fmt);
+    len = vsnprintf(NULL, 0, fmt, args);
+    va_end(args);
+
+    // Write string to buffer
+    va_start(args, fmt);
+    len += 1;
+    buf = evq_malloc(len);
+    vsnprintf(buf, len, fmt, args);
+    va_end(args);
+
+    // Flush log
+    evq_flush_log(buf, len);
+
+    evq_free(buf);
 }
 
 void __attribute__((weak))
--- src/include/evq/evq_port.h
+++ src/include/evq/evq_port.h
@@ -26,7 +26,7 @@ extern "C"
     void *evq_malloc(uint32_t size);
     void *evq_calloc(uint32_t nmemb, uint32_t size);
     void *evq_realloc(void *ptr, uint32_t size);
-    void evq_free(void *ptr);
+    void  evq_free(void *ptr);
     /** @}*/
 
 #if defined(EVQ_RTOS_SUPPORT)
@@ -35,25 +35,29 @@ extern "C"
      * @{
      */
     typedef void *evq_mutex_t;
-    evq_status_t evq_mutex_create(evq_mutex_t *mutex);
-    evq_status_t evq_mutex_destroy(evq_mutex_t mutex);
-    evq_status_t evq_mutex_lock(evq_mutex_t mutex, uint32_t timeout);
-    evq_status_t evq_mutex_lock_isr(evq_mutex_t mutex, uint32_t timeout);
-    evq_status_t evq_mutex_unlock(evq_mutex_t mutex);
-    evq_status_t evq_mutex_unlock_isr(evq_mutex_t mutex);
+    evq_status_t  evq_mutex_create(evq_mutex_t *mutex);
+    evq_status_t  evq_mutex_destroy(evq_mutex_t mutex);
+    evq_status_t  evq_mutex_lock(evq_mutex_t mutex, uint32_t timeout);
+    evq_status_t  evq_mutex_lock_isr(evq_mutex_t mutex, uint32_t timeout);
+    evq_status_t  evq_mutex_unlock(evq_mutex_t mutex);
+    evq_status_t  evq_mutex_unlock_isr(evq_mutex_t mutex);
 
     typedef void *evq_egroup_t;
-    evq_status_t evq_egroup_create(evq_egroup_t *egroup);
-    evq_status_t evq_egroup_destroy(evq_egroup_t egroup);
-    evq_status_t evq_egroup_set(evq_egroup_t egroup, uint32_t flags, uint32_t timeout);
-    evq_status_t evq_egroup_set_isr(evq_egroup_t egroup, uint32_t flags, uint32_t timeout);
-    evq_status_t evq_egroup_wait(evq_egroup_t egroup, uint32_t flags, uint32_t *matchFlag, bool waitForAll, uint32_t timeout);
+    evq_status_t  evq_egroup_create(evq_egroup_t *egroup);
+    evq_status_t  evq_egroup_destroy(evq_egroup_t egroup);
+    evq_status_t  evq_egroup_set(evq_egroup_t egroup, uint32_t flags, uint32_t timeout);
+    evq_status_t  evq_egroup_set_isr(evq_egroup_t egroup, uint32_t flags, uint32_t timeout);
+    evq_status_t  evq_egroup_wait(evq_egroup_t egroup,
+                                  uint32_t     flags,
+                                  uint32_t    *matchFlag,
+                                  bool         waitForAll,
+                                  uint32_t     timeout);
     /** @} */
 #endif // EVQ_RTOS_SUPPORT
 
-    /**
-     * @brief Queue wrappers
-    */
+       /**
+        * @brief Queue wrappers
+        */
 
     // typedef void *evq_queue_t;
     // evq_status_t evq_queue_create(evq_queue_t *queue, uint16_t entrySize, uint16_t queueSize);
@@ -66,9 +70,16 @@ extern "C"
     /**
      * @brief Timekeeping wrappers
      * @{
-    */
-   uint32_t evq_get_time();
-   /** @}*/
+     */
+    uint32_t evq_get_time();
+    /** @}*/
+
+    /**
+     * @brief Logging wrappers
+     * @{
+     */
+    void evq_flush_log(const char *str, uint32_t len);
+    /** @}*/
 
 #ifdef __cplusplus
 }
